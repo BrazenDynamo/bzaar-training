61aa4ce34e0ad383c61a57ff30bf759bc19b9d98
Benjamin Ryan Mendoza <brb.mendoza@gmail.com>
2020-03-18 12:57:55 +0800

    Add props to root component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App({ Component }) {
-  return (<Component></Component>);
+export default function App({ Component, pageProps }) {
+  return (<Component {...pageProps}></Component>);
 }
